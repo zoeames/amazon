eab7b36c059931ba9660ca5c8fd577e743da269d
Zoe Ames <zoeames@gmail.com>
2014-09-02 14:44:19 -0500

    flash message added

--- app/lib/security.js
+++ app/lib/security.js
@@ -2,6 +2,16 @@
 
 exports.locals = function(req, res, next){
   res.locals.user = req.user;
+
+  var keys = Object.keys(req.session.flash || {});
+  res.locals.flash={};
+  keys.forEach(function(key){
+    res.locals.flash[key] = [];
+    req.session.flash[key].forEach(function(msg){
+      res.locals.flash[key].push(req.flash(key));
+    });
+  });
+
   next();
 };
 
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,6 +7,7 @@ var morgan         = require('morgan'),
     session        = require('express-session'),
     RedisStore     = require('connect-redis')(session),
     passport       = require('passport'),
+    flash          = require('connect-flash'),
     passportConfig = require('../lib/passport/config'),
     security       = require('../lib/security'),
     debug          = require('../lib/debug'),
@@ -20,6 +21,7 @@ module.exports = function(app, express){
   app.use(bodyParser.urlencoded({extended:true}));
   app.use(methodOverride());
   app.use(session({store:new RedisStore(), secret:'my super secret key', resave:true, saveUninitialized:true, cookie:{maxAge:null}}));
+  app.use(flash());
   passportConfig(passport, app);
 
   app.use(security.locals);
@@ -29,7 +31,7 @@ module.exports = function(app, express){
   app.get('/register', users.new);
   app.post('/register', users.create);
   app.get('/login', users.login);
-  app.post('/login', passport.authenticate('local', {successRedirect:'/', failureRedirect:'/login'}));
+  app.post('/login', passport.authenticate('local', {successRedirect:'/', failureRedirect:'/login', successFlash:'Congrats!', failureFlash:'WRONG!'}));
 
   app.use(security.bounce);
   app.delete('/logout', users.logout);
